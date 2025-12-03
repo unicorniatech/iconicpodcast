d36459026703e75fba7175e80fd9d0740a2e9f6a
hildealeman <37812451+hildealeman@users.noreply.github.com>
2025-12-03 14:23:19 -0600

    Fix allowFullScreen React warning
    - Remove allowFullScreen attribute from iframes
    - Use 'allow' attribute with 'fullscreen' instead
    - Fixes: 'Allow attribute will take precedence over allowfullscreen'

--- App.tsx
+++ App.tsx
@@ -375,7 +375,6 @@ const EpisodeDetail: React.FC = () => {
                             width="100%" 
                             height="152" 
                             frameBorder="0" 
-                            allowFullScreen 
                             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
                             loading="lazy"
                             title="Spotify Player"
--- components/common/LazyYouTubePlayer.tsx
+++ components/common/LazyYouTubePlayer.tsx
@@ -38,9 +38,8 @@ export const LazyYouTubePlayer: React.FC<LazyYouTubePlayerProps> = ({ videoId, t
                 <iframe 
                     src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1`}
                     className="w-full h-full"
-                    allowFullScreen
                     title={title}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
                 />
             ) : (
                 <>
